57ec45ae989694a142533b73fe40de70be2976c2
Michele Maffei <Muffin@MacBook-Pro-di-Michele.local>
2015-03-03 13:11:17 +0100

    Initial Commit

---
+++ energymonitorwriteonfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  energymonitorwriteonfile
+//
+//  Created by Michele Maffei on 03/03/15.
+//  Copyright (c) 2015 Michele Maffei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ energymonitorwriteonfile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  energymonitorwriteonfile
+//
+//  Created by Michele Maffei on 03/03/15.
+//  Copyright (c) 2015 Michele Maffei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
